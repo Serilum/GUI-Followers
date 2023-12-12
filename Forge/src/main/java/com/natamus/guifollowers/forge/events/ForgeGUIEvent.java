4282b03ec6d174e3fa6e1421de879d421b784618
Rick South <rick@natamus.com>
2023-12-12 10:00:43 +0100

    GUI Followers source code for 1.20.0.

---
+++ Forge/src/main/java/com/natamus/guifollowers/forge/events/ForgeGUIEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.guifollowers.forge.events;
+
+import com.natamus.guifollowers.events.GUIEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeGUIEvent extends Gui {
+	public ForgeGUIEvent(Minecraft mc, ItemRenderer itemRenderer){
+		super(mc, itemRenderer);
+	}
+	
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public void renderOverlay(RenderGuiOverlayEvent.Pre e){
+		GUIEvent.renderOverlay(e.getGuiGraphics(), e.getPartialTick());
+	}
+}
