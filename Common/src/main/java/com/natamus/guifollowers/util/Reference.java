351590af64346a8a6b454ad8b4f5bbe837a1044f
Rick South <rick@natamus.com>
2024-01-01 21:26:18 +0100

    (v3.5) Updated GUI Followers to work with the latest NeoForge update.

--- Common/src/main/java/com/natamus/guifollowers/util/Reference.java
+++ Common/src/main/java/com/natamus/guifollowers/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.guifollowers.util;
 public class Reference {
 	public static final String MOD_ID = "guifollowers";
 	public static final String NAME = "GUI Followers";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
