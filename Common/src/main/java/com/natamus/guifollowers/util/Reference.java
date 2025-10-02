7605c05853d6fcfd8376cbc9ffdd16d245e25676
Rick South <rick@natamus.com>
2025-10-03 01:14:47 +0200

    (v4.1) Updated mod to use Collective's new RegisterKeyMappingsHelper.

--- Common/src/main/java/com/natamus/guifollowers/util/Reference.java
+++ Common/src/main/java/com/natamus/guifollowers/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.guifollowers.util;
 public class Reference {
 	public static final String MOD_ID = "guifollowers";
 	public static final String NAME = "GUI Followers";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public static final String ACCEPTED_VERSIONS = "[1.21.8]";
 }
