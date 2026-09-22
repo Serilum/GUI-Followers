2992e24bc989805ab183dd4b138cde845eee3476
Rick South <rick@natamus.com>
2026-09-22 16:42:36 +0200

    Updated GUI Followers to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/guifollowers/util/Reference.java
+++ Common/src/main/java/com/natamus/guifollowers/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "guifollowers";
 	public static final String NAME = "GUI Followers";
 	public static final String VERSION = "4.3";
-	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
+	public static final String ACCEPTED_VERSIONS = "[26.3.0]";
 }
